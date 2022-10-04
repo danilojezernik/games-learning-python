fef02a41a49c46bb3bd5915df7e34193de8cef1f
danilojezernik <dani.jezernik@gmail.com>
2022-10-04 14:44:41 +0200

    playing with computer

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,33 +1,56 @@
 from random import randint
+player_wins = 0
+computer_wins = 0
+winning_score = 3
 
-player = input('Player 1, make your move: ').lower()
+while player_wins < winning_score and computer_wins < winning_score:
+    print(f"Player Score: {player_wins} || Computer Score: {computer_wins}")
+    print("...rock...")
+    print("...paper...")
+    print("...scissors...")
 
-rand_num = randint(0, 2)
+    player = input("(Enter your choice): ").lower()
+    if player == "quit" or player == "q":
+        break
+    random_num = randint(0, 2)
+    if random_num == 0:
+        computer = "rock"
+    elif random_num == 1:
+        computer = "paper"
+    else:
+        computer = "scissors"
 
-if rand_num == 0:
-    computer = 'rock'
-elif rand_num == 1:
-    computer = 'paper'
+    print(f"The computer plays: {computer}")
+
+    if player == computer:
+        print("It's a tie!")
+    elif player == "rock":
+        if computer == "paper":
+            print("Computer wins :( ")
+            computer_wins += 1
+        else:
+            print("Player wins!")
+            player_wins += 1
+    elif player == "paper":
+        if computer == "rock":
+            print("Player win!")
+            player_wins += 1
+        else:
+            print("Computer wins!")
+            computer_wins += 1
+    elif player == "scissors":
+        if computer == "rock":
+            print("Computer wins!")
+            computer_wins += 1
+        else:
+            print("You win!")
+            player_wins += 1
+    else:
+        print("Please enter a valid move!")
+
+if player_wins > computer_wins:
+    print("CONGRATS, YOU WON!")
+elif player_wins == computer_wins:
+    print("IT'S A TIE")
 else:
-    computer = 'scissors'
-print(f'Computer plays: {computer}')
-
-if player == computer:
-    print('it\'s a tie!')
-elif player == 'rock':
-    if computer == 'scissors':
-        print('Player wins!')
-    else:
-        print('Computer wins!')
-elif player == 'paper':
-    if computer == 'rock':
-        print('Player wins!')
-    else:
-        print('Computer wins!')
-elif player == 'scissors':
-    if computer == 'rock':
-        print('Computer wins!')
-    else:
-        print('Player wins!')
-else:
-    print(f'{player} <-- is not a valid move. Use rock, paper or scissors instead!')
+    print("OH NO :( THE COMPUTER WON...")
